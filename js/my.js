027714541afa04b5dfb0b5d9545f9628c243d544
ya-tien hsin <a9911006@gmail.com>
2021-03-03 13:49:46 +0800

    test1

---
+++ js/my.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+
+    //AOS
+    $(function () {
+        AOS.init();
+    })
+
+    //simple pagination
+    var items = $(".list-wrapper .list-item");
+    var numItems = items.length;
+    var perPage = 10;
+    var html = $('html,body');
+
+    items.slice(perPage).hide();
+
+    $('#pagination-container').pagination({
+        items: numItems,
+        itemsOnPage: perPage,
+        prevText: "上一頁",
+        nextText: "下一頁",
+        onPageClick: function (pageNumber) {
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
+            items.hide().slice(showFrom, showTo).show();
+            html.animate({
+                scrollTop: 200
+            }, 1000);
+        }
+    });
+
+
+    //漢堡選單
+    $('.nav-hamburger').click(function () {
+        $('.nav-menu').toggleClass('active');
+        $('.nav-hamburger').toggleClass('active');
+    })
+
+    //Tab
+    $(function () {
+        $(".about .btn").eq(0).addClass('active');
+        $(".about-content li").eq(0).show().siblings().hide();
+        $(".about .btn").on('click', function () {
+            var $tabIndex = $(this).index();
+            $(this).addClass('active').siblings().removeClass('active');
+            $(".about-content li").eq($tabIndex).addClass('active').siblings().removeClass('active');
+            $(".about-content li").eq($tabIndex).show().siblings().hide();
+        });
+    });
+
+    //goTop
+    $('.goTop').click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.goTop').fadeIn();
+        } else {
+            $('.goTop').stop().fadeOut();
+        }
+
+    });
+
+    //table
+    if ($(".popup-block p").eq(3).text() == "") {
+        $(".popup-block").eq(3).hide();
+    }
+
+    //常見問題
+    $('.qa-list .qa .qa-content').slideUp();
+    $('.qa-list .qa').click(function () {
+        var $index = $(this).index();
+        $('.qa-list .qa .qa-content').eq($index).slideToggle();
+        $(this).addClass('active')
+        if ($('.qa-list .qa .plus').eq($index).html() == " + ") {
+            $('.qa-list .qa .plus').eq($index).html(" - ")
+        } else(
+            $('.qa-list .qa .plus').eq($index).html(" + ")
+        )
+    })
+
+
+})
